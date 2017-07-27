8b4c171826b289fb1897b523e40ed0f4a7eb01c7
Generosa Litton <generosa.litton@yahoo.com>
2017-07-26 17:38:19 -0700

    paws and oracle modal fix

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -2,10 +2,25 @@
 
 $(document).ready(function(){
     // Stops YouTube video when modal window is closed
-    $('.close-youtube').on('click', function(){
-        var video = $('.you-tube').attr('src');
-          $('.you-tube').attr('src','');
-          $('.you-tube').attr('src', video);
+
+    // PAWS video
+    $('#close-paws').on('click', function(){
+        var video = $('#paws').attr('src');
+          $('#paws').attr('src','');
+          $('#paws').attr('src', video);
         }
       )
-  });
+
+    // Hitachi Oracle video
+    $('#close-oracle').on('click', function(){
+        var video = $('#oracle').attr('src');
+          $('#oracle').attr('src','');
+          $('#oracle').attr('src', video);
+        }
+      )
+
+
+
+
+
+});
